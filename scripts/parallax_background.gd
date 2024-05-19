99669d32c2c37b76a4a8072a49d0d71fe19e9810
Simon <alexzandermcgraw@gmail.com>
2024-05-19 12:40:21 -0700

    background is kind of working!

--- scripts/parallax_background.gd
+++ scripts/parallax_background.gd
@@ -3,20 +3,12 @@ extends ParallaxBackground
 var camera: Camera2D
 
 func _ready():
-	# Scale sprites to match viewport size
-	_scale_sprites_to_viewport()
-	
-func _scale_sprites_to_viewport():
-	var viewport_size = get_viewport().get_visible_rect().size
-	for layer in get_children():
-		if  layer is ParallaxLayer:
-			for sprite in layer.get_children():
-				if sprite is Sprite2D:
-					var texture_size = sprite.texture.get_size()
-					var scale_x = viewport_size.x / texture_size.x
-					var scale_y = viewport_size.y / texture_size.y
-					sprite.scale = Vector2(scale_x, scale_y)
+	# Initialize and update the scroll offset based on camera position
+	_update_scroll_offset()
 
-func _process(delta):
+func _update_scroll_offset():
 	if camera:
 		self.scroll_offset.x = camera.position.x
+
+func _process(delta):
+	_update_scroll_offset()
